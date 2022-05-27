c087a807bb0f3b88f4057d793a1338f442cc9348
ShutDownMan <Jedson_Gabriel@hotmail.com>
2022-05-27 11:52:03 -0300

    sync

---
+++ src/com/jesuisjedi/GraphParser.java
@@ -0,0 +1,45 @@
+package com.jesuisjedi;
+
+import org.graphstream.graph.Edge;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.implementations.SingleGraph;
+import org.graphstream.stream.file.FileSource;
+import org.graphstream.stream.file.FileSourceDGS;
+
+import java.nio.file.Path;
+
+public class GraphParser {
+    public static Graph ParseFile(Path filePath) {
+        System.setProperty("org.graphstream.ui", "swing");
+
+        Graph graph = new SingleGraph("Tutorial 1");
+        graph.display();
+
+        graph.setAttribute("ui.quality");
+        graph.setAttribute("ui.antialias");
+
+        graph.addNode("A");
+        graph.addNode("B");
+        graph.addEdge("AB", "A", "B");
+        graph.addNode("C");
+        graph.addEdge("BC", "B", "C", true); // Directed edge.
+        graph.addEdge("CA", "C", "A");
+
+
+        Edge ab = graph.getEdge("AB");
+        Edge bc = graph.getEdge("BC");
+        Edge ca = graph.getEdge("CA");
+
+
+        ab.setAttribute("ui.label", "AB");
+        bc.setAttribute("ui.label", "BC");
+        ca.setAttribute("ui.label", "CA");
+
+        for(String id : new String[]{"A", "B", "C"}){
+            graph.getNode(id).setAttribute("ui.label", id);
+        }
+        graph.setAttribute("ui.stylesheet", "url(data/style.css);");
+
+        return graph;
+    }
+}
--- src/com/jesuisjedi/Main.java
+++ src/com/jesuisjedi/Main.java
@@ -1,30 +1,37 @@
 package com.jesuisjedi;
 
-import org.graphstream.graph.Graph;
-import org.graphstream.graph.implementations.SingleGraph;
-import org.graphstream.stream.file.FileSource;
-import org.graphstream.stream.file.FileSourceDGS;
-
-import java.io.Console;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        System.setProperty("org.graphstream.ui", "swing");
-        Graph graph = new SingleGraph("Demo2");
-        graph.setAttribute("ui.antialias");
-        graph.setAttribute("ui.quality");
-        graph.setAttribute("ui.stylesheet", "url(/home/jedi/IdeaProjects/Graph.io/data/style.css);");
+        System.out.print("Digite o caminho do arquivo a ser analisado: ");
 
-        graph.display(false);
-        FileSource source = new FileSourceDGS();
-        source.addSink( graph );
-        source.begin("/home/jedi/IdeaProjects/Graph.io/data/demo2.dgs");
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(System.in));
 
-        while(source.nextStep()){
-            System.out.println("TESTE!");
-            Thread.sleep(2000);
+        Path filePath = Paths.get(reader.readLine());
+        boolean tryAgain = true;
+        while(tryAgain && Files.notExists(filePath)) {
+            System.out.println("Este caminho n�o existe.");
+            System.out.println("Tentar novamente? [S/n]");
+
+            String choice = reader.readLine();
+
+            tryAgain = (!choice.equalsIgnoreCase("N"));
+
+            if(tryAgain) {
+                System.out.print("Escreva o caminho do arquivo novamente: ");
+                filePath = Paths.get(reader.readLine());
+            }
         }
-        source.end();
+
+        System.out.println(filePath);
+
+
     }
 }
